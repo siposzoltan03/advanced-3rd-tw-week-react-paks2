607107e82302fb62bb193364d6062113630a78f0
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-02-17 14:33:47 +0100

    Add build folder

---
+++ build/precache-manifest.0ee67feee2db3dbeb9edca25cd25c867.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d650f895098b97c335d00516d84f968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a84dcdce9f3fe3d3e7f7",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "eaeac24fa2f712ead3a3",
+    "url": "/static/js/2.9ce83bfc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9ce83bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84dcdce9f3fe3d3e7f7",
+    "url": "/static/js/main.08abcb84.chunk.js"
+  },
+  {
+    "revision": "019ffba0f40cf8b505f9",
+    "url": "/static/js/runtime-main.a90de6a5.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
