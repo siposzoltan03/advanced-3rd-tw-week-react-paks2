03a7c6f04a81ff73c81783b02216f18e5c6330ce
wory04 <voroseszterveronika@gmail.com>
2020-02-18 23:00:40 +0100

    implement getNearestCityUrl function

--- src/utility/GetData.js
+++ src/utility/GetData.js
@@ -4,10 +4,21 @@ export function getCity(url) {
   return Axios.get(url).then(resp => ({
     geonameId: resp.data.geoname_id,
     name: resp.data.name,
-    urbanArea: resp.data._links["city:urban_area"].href,
+    urbanArea: ((resp.data._links || {})["city:urban_area"] || {}).href,
     population: resp.data.population,
     latitude: resp.data.location.latlon.latitude,
     longitude: resp.data.location.latlon.longitude,
     fullName: resp.data.full_name
   }));
+}
+
+export function getNearestCityUrl(lat, lng) {
+  return Axios.get(
+    `https://api.teleport.org/api/locations/${lat},${lng}/`
+  ).then(
+    resp =>
+      resp.data._embedded["location:nearest-cities"][0]._links[
+        "location:nearest-city"
+      ].href
+  );
 }
