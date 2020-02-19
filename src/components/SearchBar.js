875897dfade6b1f73a346f1961b12d042ea0fda6
Márk Paróczi <mark.paroczi@gmail.com>
2020-02-19 14:48:40 +0100

    Add autocomplete skeleton

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,6 @@
-import React, {useEffect, useContext, useState} from "react";
+import React, { useEffect, useContext, useState, createRef } from "react";
 import { Input } from "antd";
+import TeleportAutocomplete from "teleport-autocomplete/dist/teleport-autocomplete";
 
 import { CityContext } from "../contexts/CityContext";
 import { getCity } from "../utility/GetData";
@@ -11,36 +12,46 @@ function SearchBar() {
   const [cityTwo, setCityTwo] = city2;
 
   const { Search } = Input;
+  const search = createRef();
 
   useEffect(() => {
-      if (cityUrl !== null) {
-          getCity(cityUrl).then(data =>
-              Object.keys(cityOne).length === 0
-                  ? setCityOne(data)
-                  : Object.keys(cityTwo).length === 0
-                  ? setCityTwo(data)
-                  : alert("You've already chosen two cities to compare!")
-          );
-      }
+    if (cityUrl !== null) {
+      getCity(cityUrl).then(data =>
+        Object.keys(cityOne).length === 0
+          ? setCityOne(data)
+          : Object.keys(cityTwo).length === 0
+          ? setCityTwo(data)
+          : alert("You've already chosen two cities to compare!")
+      );
+    }
   }, [cityUrl]);
 
-    function changeUrl(value) {
+  // useEffect(
+  //   TeleportAutocomplete.init(search).on("change", function(value) {
+  //     console.log(value);
+  //   })
+  // );
 
-    }
+  function changeUrl(value) {
+    setCityUrl(value);
+  }
 
   return (
-      <div>
-        <Search
-            placeholder="Search for a city"
-            onSearch={value => changeUrl(value)}
-            style={{ width: 200 }}
-            className="search-input"
-        />
-        <span>
-            {" "}
-            You've selected the following cities: {cityOne.name} {cityTwo.name}
-        </span>
-      </div>
+    <div>
+      <Search
+        placeholder="Search for a city"
+        onSearch={value => changeUrl(value)}
+        style={{ width: 200 }}
+        className="search-input"
+        autoComplete="off"
+        tabIndex="1"
+        itemRef={search}
+      />
+      <span>
+        {" "}
+        You've selected the following cities: {cityOne.name} {cityTwo.name}
+      </span>
+    </div>
   );
 }
 
