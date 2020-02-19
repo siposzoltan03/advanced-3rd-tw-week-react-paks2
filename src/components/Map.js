1b7cfee7baaf3e5cf72eef7cdd9444973b3ea514
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-02-19 13:34:38 +0100

    Implement collapsible sidebar functionality

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,27 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import L from "leaflet";
+import SideBar from "./SideBar";
+import '../Map.css'
 
 function Map() {
-  useEffect(() => {
-    // create map
-    L.map("map", {
-      center: [49.8419, 24.0315],
-      zoom: 4,
-      layers: [
-        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-          attribution:
-            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        })
-      ]
-    });
-  }, []);
+    useEffect(() => {
+        // create map
+        L.map("map", {
+            center: [49.8419, 24.0315],
+            zoom: 4,
+            layers: [
+                L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+                    attribution:
+                        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                })
+            ]
+        });
+    }, []);
 
-  return <div id="map"></div>;
+    return <div id="map">
+            <SideBar/>
+    </div>;
 }
+
 
 export default Map;
