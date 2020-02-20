4cb0bbb4cb31e645a2aa339309ca840bff21ee8c
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-02-20 22:43:01 +0100

    restore city score colors upon deletion

---
+++ src/utility/DataManipulation.js
@@ -0,0 +1,15 @@
+export const restoreCity = (city) => {
+  let cityCopy = {...city};
+  if (cityCopy.housing !== undefined) {
+    const greater = 2;
+    cityCopy["housing"][greater] = undefined;
+    cityCopy["costOfLiving"][greater] = undefined;
+    cityCopy["travelConnectivity"][greater] = undefined;
+    cityCopy["safety"][greater] = undefined;
+    cityCopy["internetAccess"][greater] = undefined;
+    cityCopy["environmentalQuality"][greater] = undefined;
+    cityCopy["leisureAndCulture"][greater] = undefined;
+    delete cityCopy.compared;
+  }
+  return cityCopy;
+};
