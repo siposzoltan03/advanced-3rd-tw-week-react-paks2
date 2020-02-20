9db9690c26b9cd68d8b51069de861db9a667b981
Nikolett Szucs <ts2jzgte@gmail.com>
2020-02-20 14:46:57 +0100

    add sidebar content

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,35 +1,38 @@
-import React, { useState } from 'react'
-import { useSpring, animated } from 'react-spring'
-import useMeasure from './useMeasure'
-import '../style.css'
-import { CityProvider } from '../contexts/CityContext'
-import Cities from '../components/Cities'
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
+import useMeasure from "./useMeasure";
+import "../style.css";
+import { CityProvider } from "../contexts/CityContext";
+import Cities from "../components/Cities";
 // import 'fontawesome';
 
 export function SideBar() {
-    const [open, toggle] = useState(false);
-    const [bind, {width}] = useMeasure();
-    const props = useSpring({width: open ? width : 20});
-    const mainProps = {width: open ? '50%' : '20px'};
+  console.log("SIDEBAAAAAAAAAAAAAAAAAAAAAR");
+  const [open, toggle] = useState(false);
+  const [bind, { width }] = useMeasure();
+  const props = useSpring({ width: open ? width : 20 });
+  const mainProps = { width: open ? "50%" : "20px" };
 
-    return (
-        <div {...bind} className="main" onClick={() => toggle(!open)} style={mainProps}>
-            <animated.div className="fill" style={props}>
-                <div className='arrow'><i className={arrowDirection()}/></div>
-            </animated.div>
-            
-            <animated.div className="content">
-							<CityProvider>
-								<Cities />
-							</CityProvider>
-            </animated.div>
+  return (
+    <div
+      {...bind}
+      className="main"
+      onClick={() => toggle(!open)}
+      style={mainProps}
+    >
+      <animated.div className="fill" style={props}>
+        <div className="arrow">
+          <i className={arrowDirection()} />
         </div>
-    );
-    function arrowDirection() {
-        return  open ? "fa fa-chevron-left" : "fa fa-chevron-right"
-    }
+      </animated.div>
+      <animated.div className="content">
+        <Cities />
+      </animated.div>
+    </div>
+  );
+  function arrowDirection() {
+    return open ? "fa fa-chevron-left" : "fa fa-chevron-right";
+  }
 }
-
-
 
 export default SideBar;
