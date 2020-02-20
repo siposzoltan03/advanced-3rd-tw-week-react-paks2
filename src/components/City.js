f724b77808321b8574cd3bd3d5d78215abc07892
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-02-20 01:40:55 +0100

    add city component skeleton

---
+++ src/components/City.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const City = () => {
+    const name = 0;
+    const score = 1;
+    return(
+        <div className="city">
+           {/* <p>{cityName}</p> */}
+        </div>
+    )
+}
+
+export default City;
